0361094effc8ccb23c5b8495f473c9021e16da48
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2020-10-26 17:34:23 +0100

    Initial idea of build

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace Personnr_01
     {
         static void Main(string[] args)
         {
+            string userInput;
+            int personalnr;
+            //User input ["method"]
+            UserInput();
+            userInput = Console.ReadLine();
+            personalnr = int.Parse(userInput);            
+            //1753-2020 [method]
+
+            //Check if month is valid [method]
+
+            //Check if day is valid for month [method]
+
+            //YYYYMMDDnnnc check if nnn is 000-999 [method]
+
+            //Check gender [method]
+
+            //Print if personal number is correct and gender [method]
+            Console.ReadKey();
+        }
+        
+        static void UserInput()
+        {
+            Console.Write("Ange ditt personnummer: ");
         }
     }
 }
